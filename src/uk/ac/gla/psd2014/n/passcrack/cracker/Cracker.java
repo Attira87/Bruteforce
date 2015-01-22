279c21a562032fe09127a19f6ab5ef03758b56ab
1106611k <1106611k@117c5071-20ca-4037-9b2d-e9391c0cf9c1>
2015-01-22 23:49:53 +0000

    Rolled back to r14

--- src/uk/ac/gla/psd2014/n/passcrack/cracker/Cracker.java
+++
@@ -1,5 +0,0 @@
-package uk.ac.gla.psd2014.n.passcrack.cracker;
-
-public interface Cracker {
-	public boolean tryPassword(String password) throws CrackerException;
-}
--- src/uk/ac/gla/psd2014/n/passcrack/scrapper/Scrapper.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.gla.psd2014.n.passcrack.scrapper;
-
-import java.util.ArrayList;
-
-public interface Scrapper {
-	public ArrayList<String> getPasswords();
-}

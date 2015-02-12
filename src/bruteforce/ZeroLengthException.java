085eac54ca3e56b1caf5c64a9e9dc8271d210873
1106611k <1106611k@117c5071-20ca-4037-9b2d-e9391c0cf9c1>
2015-02-12 01:05:23 +0000

    Adding missing ZeroLengthException file.

---
+++ src/bruteforce/ZeroLengthException.java
@@ -0,0 +1,11 @@
+package bruteforce;
+
+public class ZeroLengthException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ZeroLengthException(String message) {
+		super(message);
+	}
+
+}
